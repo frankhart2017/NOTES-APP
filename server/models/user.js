03bc1c765e62bf531b27f0ba0111e37f25161333
frankhart2017 <sdharchou@gmail.com>
2018-04-29 14:58:23 +0530

    add models and initial server file

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,32 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+var UserSchema = new Mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    trim: true
+  }
+});
+
+var User = mongoose.model('User', UserSchema);
